877d082751bcc629c257c7aee87246e0c9220dd3
Theo <theocarr777@gmail.com>
2015-07-17 01:47:07 +1200

    testing changes

--- quote.js
+++ quote.js
@@ -11,9 +11,10 @@ var cors = require('cors');
 var app = express();
 
 var quotes = [
-  { author : 'brendan fraiser', text : "REMEMBER THE ALIMONY"},
-  { author: 'cia', text: "BANE?" },
-  { author: 'le sad frog', text: "REEEEEEEEEEEEEEEEEEEE" }
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, TESTthe word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when itTEST2 happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. TESTDon’t be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you’TESTre afraid to live. What a way to exist."}
 ];
 
 // make sure we can parse JSON passed in the body or encoded into url
